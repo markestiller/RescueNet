97a06eaf4306cf80c0aefcf3b66387b94dca0ccc
markestiller <markalestiller@gmail.com>
2023-08-19 00:59:47 -0400

    Added footer

--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
 
 const About = () => {
-  return (
-    <div className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-start via-via to-end px-4">
-      WILDFIREEE
-    </div>
-  );
+    return (
+        <div className="w-full h-screen bg-black">
+            <div className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-start via-via to-end px-4">
+                About
+            </div>
+        </div>
+    );
 };
 
 export default About;
--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
 
 const footer = () => {
-    return <div>footer</div>;
+    return (
+        <div className="w-full h-screen bg-black">
+            <div className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-start via-via to-end px-4">
+                Contact
+            </div>
+        </div>
+    );
 };
 
 export default footer;
