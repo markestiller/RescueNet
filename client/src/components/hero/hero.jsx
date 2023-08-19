d8cfe8907c1fec10dd9a0422655aee6cc1c8d8ff
markestiller <markalestiller@gmail.com>
2023-08-19 01:59:32 -0400

    Completed navbar for mobile view, added dependencies for react scroll

--- client/src/components/hero/hero.jsx
+++ client/src/components/hero/hero.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
 const hero = () => {
-  return (
-    <div className="flex w-full h-screen items-center bg-black">
-      <div className="text-7xl font-bold text-transparent text-center bg-clip-text bg-gradient-to-r from-start via-via to-end px-4">
-        AAAAAAAAAAAAAAAAHHH
-      </div>
-    </div>
-  );
+    return (
+        <div name="Home" className="flex w-full h-screen items-center bg-black">
+            <div className="text-7xl font-bold text-transparent justify-center items-center bg-clip-text bg-gradient-to-r from-start via-via to-end px-4">
+                AAAAAAAAAAAAAAAAHHH
+            </div>
+        </div>
+    );
 };
 
 export default hero;
