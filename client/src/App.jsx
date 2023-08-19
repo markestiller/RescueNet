598d01c894981695c5ea37e8d69718f49ff65fbc
markestiller <markalestiller@gmail.com>
2023-08-18 22:50:57 -0400

    Second Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,20 +10,9 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Header />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="text-red-500">
+        <p>Header</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
