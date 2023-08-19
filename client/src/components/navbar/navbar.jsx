7f01e1bbd8d0f637c9b7c85f9178e90c5517390f
markestiller <markalestiller@gmail.com>
2023-08-19 01:14:53 -0400

    Navbar mobile view in progress

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
     let links = [
@@ -6,19 +7,28 @@ const Navbar = () => {
         { id: 2, name: 'About' },
         { id: 3, name: 'Contact' },
     ];
+
+    let [icon, setIcon] = useState(false);
+
     return (
         <div className="fixed flex justify-between items-center w-full h-20 z-10 bg-black">
             <div className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-start via-via to-end px-4">
                 WildFire
             </div>
             <div>
-                <ul className="flex flex-row">
+                <ul className="hidden md:flex flex-row">
                     {links.map(({ id, name }) => (
                         <li key={id} className=" font-bold text-white px-4">
                             {name}
                         </li>
                     ))}
                 </ul>
+            </div>
+            <div
+                onClick={() => setIcon(!icon)}
+                className="flex cursor-pointer md:hidden z-10 hover:scale-105 text-white px-4"
+            >
+                {icon ? <FaTimes size={20} /> : <FaBars size={20} />}
             </div>
         </div>
     );
