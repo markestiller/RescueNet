cf3f2dd2116e51c7f235666fc5ef9da7582e2a09
markestiller <markalestiller@gmail.com>
2023-08-19 00:53:21 -0400

    changes to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
-import "./App.css";
-import Navbar from "./components/navbar/navbar";
-import Footer from "./components/footer/footer";
-import Hero from "./components/hero/hero";
-import About from "./components/about/about";
+import './App.css';
+import Navbar from './components/navbar/navbar';
+import Footer from './components/footer/footer';
+import Hero from './components/hero/hero';
+import About from './components/about/about';
 
-import React from "react";
+import React from 'react';
 
 const App = () => {
-  return (
-    <div className="w-full">
-      <Navbar />
-      <Hero />
-      <About />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="w-full">
+            <Navbar />
+            <Hero />
+            <About />
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
