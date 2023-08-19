c637254596e4fc272824e0fbd2d681f0f9205581
markestiller <markalestiller@gmail.com>
2023-08-19 00:49:36 -0400

    made changes to navbar

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,27 +1,27 @@
-import React from "react";
+import React from 'react';
 
 const Navbar = () => {
-  let links = [
-    { id: 1, name: "Home" },
-    { id: 2, name: "About" },
-    { id: 3, name: "Contact" },
-  ];
-  return (
-    <div className="flex justify-between items-center w-full h-20 z-10 bg-black">
-      <div className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-start via-via to-end px-4">
-        WILDFIREEE
-      </div>
-      <div>
-        <ul className="flex flex-row">
-          {links.map(({ id, name }) => (
-            <li key={id} className=" font-bold text-white px-4">
-              {name}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+    let links = [
+        { id: 1, name: 'Home' },
+        { id: 2, name: 'About' },
+        { id: 3, name: 'Contact' },
+    ];
+    return (
+        <div className="fixed flex justify-between items-center w-full h-20 z-10 bg-black">
+            <div className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-start via-via to-end px-4">
+                WildFire
+            </div>
+            <div>
+                <ul className="flex flex-row">
+                    {links.map(({ id, name }) => (
+                        <li key={id} className=" font-bold text-white px-4">
+                            {name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
