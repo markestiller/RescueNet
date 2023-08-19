0b1161ff15f57276830b2778e90440b12c9457a0
Abrar <abrarmurad00@gmail.com>
2023-08-18 23:11:50 -0400

    fixed rendering issue : D

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 import navbar from "./components/navbar/navbar";
 import hero from "./components/hero/hero";
 import footer from "./components/footer/footer";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <div>
-                <h1 className="text-3xl font-bold underline">Hello world!</h1>
-                <Header />
+                <h1 className="text-3xl font-bold underline text-red-500">
+                    Hello world!
+                </h1>
             </div>
         </>
     );
