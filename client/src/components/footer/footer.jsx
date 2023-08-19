4becbc3a4bbc6d24b554375a1b0187b1ac8c94c2
Abrar <abrarmurad00@gmail.com>
2023-08-19 10:24:42 -0400

    fixed navbar overlaps

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const footer = () => {
     return (
-        <div name="Contact" className="w-full h-screen bg-red-100">
+        <div name="Contact" className="w-full h-40 bg-zinc-800">
             <div className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-500 px-4">
                 Contact
             </div>
