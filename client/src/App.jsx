0d52750aa9b81caca97b86682fbc63901585dd9a
Abrar <abrarmurad00@gmail.com>
2023-08-18 23:00:54 -0400

    added components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/Header";
+import navbar from "./components/navbar/navbar";
+import hero from "./components/hero/hero";
+import footer from "./components/footer/footer";
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <Header />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <h1 className="text-3xl font-bold underline">Hello world!</h1>
+                <Header />
+            </div>
+        </>
+    );
 }
 
 export default App;
