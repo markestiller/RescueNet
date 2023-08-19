1db3a880c53d9d34b8d5fd633c4d969849fdde9c
Abrar <abrarmurad00@gmail.com>
2023-08-19 13:11:59 -0400

    added wave again

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,11 +1,40 @@
 export default function Form() {
+    const subscriber = {
+        firstName: '',
+        lastName: '',
+        address: '',
+        city: '',
+        province: 'ON',
+        postalCode: '',
+        age: '',
+        phoneNumber: '',
+        occupants: 0,
+        password: '',
+    };
+
+    const homeowner = {
+        firstName: '',
+        lastName: '',
+        age: '',
+        address: '',
+        city: '',
+        province: 'AB',
+        postalCode: '',
+        phoneNumber: '',
+        capacity: 0,
+        occupants: 0,
+        password: '',
+    };
+
     return (
         <form className="flex flex-row flex-wrap w-max">
             <input
                 className="w-64 h-10 border-2 border-gray-300 rounded-md"
                 type="text"
                 placeholder="Name"
-            ></input>
+            >
+                
+            </input>
         </form>
     );
 }
--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Wave from 'react-wavify';
+
 
 const footer = () => {
     return (
