0466200ee33599618da7f511ed4b19304f7c5735
mh-anwar <mohammad.anwar2939@gmail.com>
2023-08-19 22:30:05 -0400

    Fix navbar routing

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link } from 'react-scroll';
-import { Link as RouterLink } from 'react-router-dom'; // This routes to other pages
+import { Link as RouterLink, useLocation } from 'react-router-dom'; // This routes to other pages
 const Navbar = () => {
+    let location = useLocation();
     const links = [
-        { id: 1, name: 'Home' },
-        { id: 2, name: 'About' },
-        { id: 3, name: 'Contact' },
+        { id: 1, name: 'Home', path: '/' },
+        location === '/' && { id: 2, name: 'About' },
+        location === '/' && { id: 3, name: 'Contact' },
         { id: 4, name: 'Join', path: '/auth' },
         { id: 5, name: 'Dashboard', path: '/dashboard' },
     ];
@@ -18,25 +19,32 @@ const Navbar = () => {
             name="Navbar"
             className="z-50 flex items-center justify-between w-full h-20 "
         >
-            <div className="px-4 text-4xl font-bold text-transparent animate-pulse bg-clip-text bg-gradient-to-r from-red-500 to-orange-500">
-                WildFire
-            </div>
+            <RouterLink to="/">
+                <div className="px-4 text-4xl font-bold text-transparent animate-pulse bg-clip-text bg-gradient-to-r from-red-500 to-orange-500">
+                    WildFire
+                </div>
+            </RouterLink>
             <div>
                 <ul className="flex-row hidden md:flex">
-                    {links.map(({ id, name, path }) => (
-                        <li
-                            key={id}
-                            className="px-4 font-bold text-black duration-100 cursor-pointer drop-shadow-xl hover:scale-105 "
-                        >
-                            {path ? (
-                                <RouterLink to={path}>{name}</RouterLink>
-                            ) : (
-                                <Link to={name} smooth duration={500}>
-                                    {name}
-                                </Link>
-                            )}
-                        </li>
-                    ))}
+                    {links.map(
+                        ({ id, name, path }) =>
+                            id !== undefined && (
+                                <li
+                                    key={id}
+                                    className="px-4 font-bold text-black duration-100 cursor-pointer drop-shadow-xl hover:scale-105 "
+                                >
+                                    {path ? (
+                                        <RouterLink to={path}>
+                                            {name}
+                                        </RouterLink>
+                                    ) : (
+                                        <Link to={name} smooth duration={500}>
+                                            {name}
+                                        </Link>
+                                    )}
+                                </li>
+                            )
+                    )}
                 </ul>
             </div>
             <div
