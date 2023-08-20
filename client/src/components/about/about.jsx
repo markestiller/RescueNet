70f49e9eddc78a57dbcb55c25aa6f61123b7533c
Abrar <abrarmurad00@gmail.com>
2023-08-19 23:52:05 -0400

    new push

--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -124,7 +124,7 @@ const About = () => {
                         <a
                             key={index}
                             href="#"
-                            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
                         >
                             <span className="bg-green-100 text-green-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
                                 {card.badge}
